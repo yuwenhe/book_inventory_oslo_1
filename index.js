005e6259e30a5a37914a1de202d27e06c68cd6af
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-07 10:54:34 +0100

    middleware

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+function logRequest(req, res, next) {
+	console.log('Incoming request at ' + new Date());
+	next();
+}
+
+app.use(logRequest);
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
